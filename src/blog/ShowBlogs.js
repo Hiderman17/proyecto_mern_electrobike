2ca4ef98a960d809521618cb78a29ccd49171c8c
Hiderman17 <hiderman17@gmail.com>
2022-11-29 16:54:29 -0500

    Mi primera Y ULTIMA con react

---
+++ src/blog/ShowBlogs.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+
+const URI = 'http://localhost:8000/blogs/'
+
+
+const CompShowBlogs = () => {
+    
+    const [blogs, setBlog] = useState([])
+    useEffect( ()=>{
+        getBlogs()
+    },[])
+
+    //procedimineto para mostrar todos los blogs
+    const getBlogs = async () => {
+        const res = await axios.get(URI)
+        setBlog(res.data)
+    }
+
+    //procedimineto para eliminar un blog
+    const deleteBlog = async (id) => {
+       await axios.delete(`${URI}${id}`)
+       getBlogs()
+    }
+
+    return(
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <Link to="/create" 
+                    className='btn btn-primary mt-2 mb-2'><i
+                     class="fa-solid fa-plus"></i></Link>
+                    <table className='table'>
+                        <thead className='table-primary'>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Content</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { blogs.map ( (blog) => (
+                                <tr key={ blog.id}>
+                                    <td> { blog.id } </td>
+                                    <td> { blog.title } </td>
+                                    <td> { blog.content } </td>
+                                    <td>    
+                                    <Link to={`/edit/${blog.id}`} 
+                                    className='btn btn-info'><i
+                                     className="fas fa-edit"></i></Link>
+                                        <button onClick={ ()=>deleteBlog(blog.id) } 
+                                        className='btn btn-danger'>
+                                            <i className="fa-regular fa-trash-can">
+                                                </i></button>
+                                    </td>
+                                </tr>
+                            )) }
+                        </tbody>
+                    </table>
+                </div>    
+            </div>
+        </div>
+    )
+
+}
+
+export default CompShowBlogs
+
+
+//No me miren que estoy nervioso :(
